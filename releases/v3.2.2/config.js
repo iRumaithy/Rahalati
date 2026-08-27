7ad5f48fb0000121ce8ae83c48eb0ac9ddfaf514
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-27 19:37:07 +0400

    Add files via upload

---
+++ releases/v3.2.2/config.js
@@ -0,0 +1,15 @@
+window.RAHALATI_CONFIG = Object.freeze({
+  appName: 'رحلاتي',
+  appVersion: '3.2.2',
+  supabaseUrl: 'https://itcbahydyqhlybofcyuh.supabase.co',
+  supabasePublishableKey: 'sb_publishable_qL-6DKVkAc3XWJ7JH-p2_A_-8myYRgp',
+  vapidPublicKey: 'BO7UaVLOILAwgUF5mN-kle6lCGsCyxbG1GU97YM8Qs0Bl2Qv2JqDwmNgo_mLjfz7odi3YHEtlmzMdgfSyWNZc6o',
+  functions: {
+    login: 'rahalati-login',
+    adminUsers: 'rahalati-admin-users',
+    releaseManager: 'rahalati-release-manager',
+    destinationSuggestions: 'rahalati-destination-suggestions',
+    pushNotifications: 'rahalati-push-notifications'
+  }
+});
+
