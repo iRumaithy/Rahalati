823f7bf49ec74f0e5885e32a71922463149fc337
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-27 07:34:20 +0400

    Add files via upload

--- config.js
+++ config.js
@@ -10,3 +10,6 @@ window.RAHALATI_CONFIG = Object.freeze({
     destinationSuggestions: 'rahalati-destination-suggestions'
   }
 });
+
+// Load the release/update bridge separately so stable builds can move between immutable version paths.
+(()=>{const s=document.createElement('script');s.type='module';s.src='./release-bridge.js';document.head.appendChild(s)})();
